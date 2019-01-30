6520a263ac1fbb92c3f38140b05e37439d5588ee
aq17 <aqneco@gmail.com>
2019-01-30 19:15:22 +0900

    manifest

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,37 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        anmitsu
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+  <section id="container">
+    <h1>Anmitsu</h1>
+    <div class="manifest">
+      <h2>Step1. manifest.jsonの作成</h2>
+      <p>manifest.jsonは、アドオンの情報やバージョンを記述する、アドオンの構成上必要なファイルです。</p>
+      <p>
+        <label><input type="radio" name="addonType" v-model="picked">リソースパック</label>
+        <label><input type="radio" name="addonType" v-model="picked">ビヘイビアパック</label>
+        <label><input type="radio" name="addonType" v-model="picked">ScriptingAPI</label>
+      </p>
+      <h4>アドオン名</h4>
+      <input v-model="addonName" placeholder="アドオン名を入力">
+      <h4>アドオンの説明</h4>
+      <textarea v-model="addonDescription" placeholder="説明を入力"></textarea>
+      <h4>アドオンのバージョン</h4>
+      <p><input type="number" max="9" min="0" v-model="v0">.<input type="number" max="9" min="0" v-model="v1">.<input type="number" max="9" min="0" v-model="v2"></p>
+      <button>生成</button>
+      <h4>出力</h4>
+      <p><textarea v-model="output"></textarea></p>
     </div>
+
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
-export default {
-  components: {
-    AppLogo
-  }
-}
+  // var app = new Vue({
+  //   // el: '#container',
+  //   // data:{
+  //   //   output: ''
+  //   // }
+  // })
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
-
